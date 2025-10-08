03f9fb39cc86ae9bb68f7bb29e4e6825ce7a1ad0
hoksh <hokshmax@gmail.com>
2025-10-09 00:23:09 +0300

    new update

--- app/Http/Controllers/API/v1/AppController.php
+++ app/Http/Controllers/API/v1/AppController.php
@@ -57,8 +57,11 @@ class AppController extends Controller
     {
         $data = Category::query()
             ->where('status', 'active')
-            ->whereHas('products')  // Categories that have ANY products
+            ->whereHas('products', function($query) {
+                $query->where('status', 'active');
+            })
             ->get();
+
 
         $message = __('api.ok');
         return response()->json(['status' => true, 'code' => 200, 'message' => $message, 'items' => $data]);
